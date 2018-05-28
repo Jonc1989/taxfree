16f03cbd5f594e388786678272c0eb90e2d924e2
Janis <janis.mozais@inbox.lv>
2018-05-28 19:25:05 +0300

    ...

--- wp-content/themes/custom/partials/about.php
+++ wp-content/themes/custom/partials/about.php
@@ -1,8 +1,13 @@
 <div id="about" class="col-xs-12 clear-pads">
-	<div class="col-md-6">
-		Info
-	</div>
-	<div class="col-md-6">
-		Picture
-	</div>
+    <div class="container">
+        <div class="col-md-6">
+            <h1 class="title">ЧТО ТАКОЕ TAX FREE BLOCKCHAIN</h1>
+            <p>Децентрализованная tax free маркет платформа
+                на blockchain ethereum с tax free сервисом,
+                на базе мобильного приложения. </p>
+        </div>
+        <div class="col-md-6">
+            <img src="<?php bloginfo('template_directory');?>/resources/images/circle_icon.png">
+        </div>
+    </div>
 </div>
