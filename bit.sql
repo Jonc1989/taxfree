e0d9d1dc0f99bf6e283158cfffaa8f7b90e2595e
Janis Mozais <janis.mozais@inbox.lv>
2018-06-01 17:22:59 +0300

    ...

--- bit.sql
+++ bit.sql
@@ -97,7 +97,7 @@ CREATE TABLE IF NOT EXISTS `wp_options` (
   `autoload` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'yes',
   PRIMARY KEY (`option_id`),
   UNIQUE KEY `option_name` (`option_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=284 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=287 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Dumping data for table taxfree.wp_options: ~141 rows (approximately)
 /*!40000 ALTER TABLE `wp_options` DISABLE KEYS */;
@@ -211,7 +211,7 @@ INSERT INTO `wp_options` (`option_id`, `option_name`, `option_value`, `autoload`
 	(107, 'widget_tag_cloud', 'a:1:{s:12:"_multiwidget";i:1;}', 'yes'),
 	(108, 'widget_nav_menu', 'a:1:{s:12:"_multiwidget";i:1;}', 'yes'),
 	(109, 'widget_custom_html', 'a:1:{s:12:"_multiwidget";i:1;}', 'yes'),
-	(110, 'cron', 'a:5:{i:1527851607;a:1:{s:34:"wp_privacy_delete_old_export_files";a:1:{s:32:"40cd750bba9870f18aada2478b24840a";a:3:{s:8:"schedule";s:6:"hourly";s:4:"args";a:0:{}s:8:"interval";i:3600;}}}i:1527855207;a:3:{s:16:"wp_version_check";a:1:{s:32:"40cd750bba9870f18aada2478b24840a";a:3:{s:8:"schedule";s:10:"twicedaily";s:4:"args";a:0:{}s:8:"interval";i:43200;}}s:17:"wp_update_plugins";a:1:{s:32:"40cd750bba9870f18aada2478b24840a";a:3:{s:8:"schedule";s:10:"twicedaily";s:4:"args";a:0:{}s:8:"interval";i:43200;}}s:16:"wp_update_themes";a:1:{s:32:"40cd750bba9870f18aada2478b24840a";a:3:{s:8:"schedule";s:10:"twicedaily";s:4:"args";a:0:{}s:8:"interval";i:43200;}}}i:1527855236;a:2:{s:19:"wp_scheduled_delete";a:1:{s:32:"40cd750bba9870f18aada2478b24840a";a:3:{s:8:"schedule";s:5:"daily";s:4:"args";a:0:{}s:8:"interval";i:86400;}}s:25:"delete_expired_transients";a:1:{s:32:"40cd750bba9870f18aada2478b24840a";a:3:{s:8:"schedule";s:5:"daily";s:4:"args";a:0:{}s:8:"interval";i:86400;}}}i:1527859118;a:1:{s:30:"wp_scheduled_auto_draft_delete";a:1:{s:32:"40cd750bba9870f18aada2478b24840a";a:3:{s:8:"schedule";s:5:"daily";s:4:"args";a:0:{}s:8:"interval";i:86400;}}}s:7:"version";i:2;}', 'yes'),
+	(110, 'cron', 'a:4:{i:1527855207;a:4:{s:16:"wp_version_check";a:1:{s:32:"40cd750bba9870f18aada2478b24840a";a:3:{s:8:"schedule";s:10:"twicedaily";s:4:"args";a:0:{}s:8:"interval";i:43200;}}s:17:"wp_update_plugins";a:1:{s:32:"40cd750bba9870f18aada2478b24840a";a:3:{s:8:"schedule";s:10:"twicedaily";s:4:"args";a:0:{}s:8:"interval";i:43200;}}s:16:"wp_update_themes";a:1:{s:32:"40cd750bba9870f18aada2478b24840a";a:3:{s:8:"schedule";s:10:"twicedaily";s:4:"args";a:0:{}s:8:"interval";i:43200;}}s:34:"wp_privacy_delete_old_export_files";a:1:{s:32:"40cd750bba9870f18aada2478b24840a";a:3:{s:8:"schedule";s:6:"hourly";s:4:"args";a:0:{}s:8:"interval";i:3600;}}}i:1527855236;a:2:{s:19:"wp_scheduled_delete";a:1:{s:32:"40cd750bba9870f18aada2478b24840a";a:3:{s:8:"schedule";s:5:"daily";s:4:"args";a:0:{}s:8:"interval";i:86400;}}s:25:"delete_expired_transients";a:1:{s:32:"40cd750bba9870f18aada2478b24840a";a:3:{s:8:"schedule";s:5:"daily";s:4:"args";a:0:{}s:8:"interval";i:86400;}}}i:1527859118;a:1:{s:30:"wp_scheduled_auto_draft_delete";a:1:{s:32:"40cd750bba9870f18aada2478b24840a";a:3:{s:8:"schedule";s:5:"daily";s:4:"args";a:0:{}s:8:"interval";i:86400;}}}s:7:"version";i:2;}', 'yes'),
 	(111, 'theme_mods_twentyseventeen', 'a:2:{s:18:"custom_css_post_id";i:-1;s:16:"sidebars_widgets";a:2:{s:4:"time";i:1527164276;s:4:"data";a:4:{s:19:"wp_inactive_widgets";a:0:{}s:9:"sidebar-1";a:6:{i:0;s:8:"search-2";i:1;s:14:"recent-posts-2";i:2;s:17:"recent-comments-2";i:3;s:10:"archives-2";i:4;s:12:"categories-2";i:5;s:6:"meta-2";}s:9:"sidebar-2";a:0:{}s:9:"sidebar-3";a:0:{}}}}', 'yes'),
 	(130, 'can_compress_scripts', '1', 'no'),
 	(140, 'current_theme', 'TaxFree', 'yes'),
@@ -241,8 +241,8 @@ INSERT INTO `wp_options` (`option_id`, `option_name`, `option_value`, `autoload`
 	(270, '_site_transient_update_core', 'O:8:"stdClass":4:{s:7:"updates";a:1:{i:0;O:8:"stdClass":10:{s:8:"response";s:6:"latest";s:8:"download";s:59:"https://downloads.wordpress.org/release/wordpress-4.9.6.zip";s:6:"locale";s:5:"en_US";s:8:"packages";O:8:"stdClass":5:{s:4:"full";s:59:"https://downloads.wordpress.org/release/wordpress-4.9.6.zip";s:10:"no_content";s:70:"https://downloads.wordpress.org/release/wordpress-4.9.6-no-content.zip";s:11:"new_bundled";s:71:"https://downloads.wordpress.org/release/wordpress-4.9.6-new-bundled.zip";s:7:"partial";b:0;s:8:"rollback";b:0;}s:7:"current";s:5:"4.9.6";s:7:"version";s:5:"4.9.6";s:11:"php_version";s:5:"5.2.4";s:13:"mysql_version";s:3:"5.0";s:11:"new_bundled";s:3:"4.7";s:15:"partial_version";s:0:"";}}s:12:"last_checked";i:1527838252;s:15:"version_checked";s:5:"4.9.6";s:12:"translations";a:1:{i:0;a:7:{s:4:"type";s:4:"core";s:4:"slug";s:7:"default";s:8:"language";s:5:"de_DE";s:7:"version";s:5:"4.9.6";s:7:"updated";s:19:"2018-05-31 19:43:07";s:7:"package";s:64:"https://downloads.wordpress.org/translation/core/4.9.6/de_DE.zip";s:10:"autoupdate";b:1;}}}', 'no'),
 	(271, '_site_transient_update_themes', 'O:8:"stdClass":4:{s:12:"last_checked";i:1527848428;s:7:"checked";a:1:{s:6:"custom";s:0:"";}s:8:"response";a:1:{s:6:"custom";a:4:{s:5:"theme";s:6:"custom";s:11:"new_version";s:5:"0.5.3";s:3:"url";s:36:"https://wordpress.org/themes/custom/";s:7:"package";s:54:"https://downloads.wordpress.org/theme/custom.0.5.3.zip";}}s:12:"translations";a:0:{}}', 'no'),
 	(272, '_site_transient_update_plugins', 'O:8:"stdClass":5:{s:12:"last_checked";i:1527838253;s:7:"checked";a:2:{s:34:"advanced-custom-fields-pro/acf.php";s:5:"5.2.3";s:27:"qtranslate-x/qtranslate.php";s:7:"3.4.6.8";}s:8:"response";a:0:{}s:12:"translations";a:1:{i:0;a:7:{s:4:"type";s:6:"plugin";s:4:"slug";s:12:"qtranslate-x";s:8:"language";s:5:"de_DE";s:7:"version";s:7:"3.4.6.8";s:7:"updated";s:19:"2018-02-28 13:32:01";s:7:"package";s:81:"https://downloads.wordpress.org/translation/plugin/qtranslate-x/3.4.6.8/de_DE.zip";s:10:"autoupdate";b:1;}}s:9:"no_update";a:1:{s:27:"qtranslate-x/qtranslate.php";O:8:"stdClass":9:{s:2:"id";s:26:"w.org/plugins/qtranslate-x";s:4:"slug";s:12:"qtranslate-x";s:6:"plugin";s:27:"qtranslate-x/qtranslate.php";s:11:"new_version";s:7:"3.4.6.8";s:3:"url";s:43:"https://wordpress.org/plugins/qtranslate-x/";s:7:"package";s:63:"https://downloads.wordpress.org/plugin/qtranslate-x.3.4.6.8.zip";s:5:"icons";a:1:{s:7:"default";s:63:"https://s.w.org/plugins/geopattern-icon/qtranslate-x_f9f9f9.svg";}s:7:"banners";a:2:{s:2:"2x";s:68:"https://ps.w.org/qtranslate-x/assets/banner-1544x500.png?rev=1286019";s:2:"1x";s:67:"https://ps.w.org/qtranslate-x/assets/banner-772x250.png?rev=1286019";}s:11:"banners_rtl";a:0:{}}}}', 'no'),
-	(282, '_site_transient_timeout_theme_roots', '1527850227', 'no'),
-	(283, '_site_transient_theme_roots', 'a:1:{s:6:"custom";s:7:"/themes";}', 'no');
+	(284, '_site_transient_timeout_theme_roots', '1527852865', 'no'),
+	(285, '_site_transient_theme_roots', 'a:1:{s:6:"custom";s:7:"/themes";}', 'no');
 /*!40000 ALTER TABLE `wp_options` ENABLE KEYS */;
 
 -- Dumping structure for table taxfree.wp_postmeta
@@ -377,7 +377,7 @@ INSERT INTO `wp_postmeta` (`meta_id`, `post_id`, `meta_key`, `meta_value`) VALUE
 	(143, 46, '_edit_last', '1'),
 	(144, 46, '_edit_lock', '1527848414:1'),
 	(145, 47, '_edit_last', '1'),
-	(146, 47, '_edit_lock', '1527848571:1');
+	(146, 47, '_edit_lock', '1527849451:1');
 /*!40000 ALTER TABLE `wp_postmeta` ENABLE KEYS */;
 
 -- Dumping structure for table taxfree.wp_posts
