a6eda10938deaa03679b7e069b972078a621666f
Janis Mozais <janis.mozais@inbox.lv>
2018-06-11 17:31:40 +0300

    ...

--- wp-content/themes/custom/partials/roadmap.php
+++ wp-content/themes/custom/partials/roadmap.php
@@ -4,36 +4,39 @@
 		<h1 class="title white text-center"><?php echo __( 'Дорожная карта', 'taxfree' ) ?></h1>
 		<div class="col-xs-12 clear-pads roadmap-bg">
 			<div id="roadmap" class="col-xs-12 clear-pads">
-				<!--            <div class="container clear-pads">-->
-				<!--                <div class="item">-->
-				<!--                    создана-->
-				<!--                    концепция-->
-				<!--                    проекта-->
-				<!--                </div>-->
-				<!---->
-				<!--                <div class="item">развитие-->
-				<!--                    платформы </div>-->
-				<!--                <div class="item">утверждена-->
-				<!--                    модель-->
-				<!--                    токена</div>-->
-				<!--                <div class="item">закрытое-->
-				<!--                    тестирование-->
-				<!--                    платформы</div>-->
-				<!--                <div class="item">закрытое-->
-				<!--                    тестирование-->
-				<!--                    токена</div>-->
-				<!--                <div class="item">pre ico.-->
-				<!--                    60 дней </div>-->
-				<!--                <div class="item">публичное-->
-				<!--                    ico  60 дней</div>-->
-				<!--                <div class="item">вывод-->
-				<!--                    средств </div>-->
-				<!--                <div class="item">тестирование-->
-				<!--                    приложения для-->
-				<!--                    Евросоюза и России</div>-->
-				<!--                <div class="item">запуск-->
-				<!--                    приложения</div>-->
-				<!--            </div>-->
+				            <div class="container clear-pads">
+				                <div class="item one">
+				                    создана
+				                    концепция
+				                    проекта
+				                </div>
+
+				                <div class="item two">развитие
+				                    платформы </div>
+
+				                <div class="item three">утверждена
+				                    модель
+				                    токена</div>
+
+				                <div class="item four">закрытое
+				                    тестирование
+				                    платформы</div>
+
+				                <div class="item five">закрытое
+				                    тестирование
+				                    токена</div>
+				                <div class="item six">pre ico.
+				                    60 дней </div>
+				                <div class="item seven">публичное
+				                    ico  60 дней</div>
+				                <div class="item eight">вывод
+				                    средств </div>
+				                <div class="item nine">тестирование
+				                    приложения для
+				                    Евросоюза и России</div>
+				                <div class="item ten">запуск
+				                    приложения</div>
+				            </div>
 
 			</div>
 			<?php get_template_part('partials/team');?>
